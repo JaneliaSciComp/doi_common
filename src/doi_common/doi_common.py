690bbed72060a63e7474aed6bcd3397a09bcdfa7
Rob Svirskas <robsvi@gmail.com>
2025-01-30 16:05:47 -0500

    Fixed short format citation

--- src/doi_common/doi_common.py
+++ src/doi_common/doi_common.py
@@ -395,7 +395,7 @@ def get_journal(rec, full=True):
                 journal = rec['institution'][0]['name']
             else:
                 journal = rec['institution']['name']
-        elif 'elife' in rec['DOI'] and rec['subtype'] == 'preprint':
+        elif 'elife' in rec['DOI'] and 'subtype' in rec and rec['subtype'] == 'preprint':
             journal = 'eLife'
         elif 'osf.io' in rec['DOI']:
             journal = 'osf.io'
